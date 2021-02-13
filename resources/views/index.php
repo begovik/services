62ebedac8da7b6216c7aac24a97859abee8c0f97
begvik <amongmediator@gmail.com>
2021-02-13 03:35:38 +0200

    FC

---
+++ resources/views/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <!-- Styles -->
+    <link href="./css/app.css" rel="stylesheet">
+    <link href="./css/style.css" rel="stylesheet">
+    <title>Services</title>
+</head>
+<body>
+
+<div>
+    <?php echo __DIR__; ?>
+</div>
+
+<!-- Scripts -->
+<script src="./js/app.js" defer></script>
+</body>
+</html>
